4267194bd69130f2ae76e03fa1c9114b5eda7a71
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-05-29 22:40:28 +0200

    Dogfood the compiler plugin in internal specs

--- spokk-compiler-plugin/src/main/kotlin/io/github/pshevche/spokk/plugin/SpokkIrGenerationExtension.kt
+++ spokk-compiler-plugin/src/main/kotlin/io/github/pshevche/spokk/plugin/SpokkIrGenerationExtension.kt
@@ -3,13 +3,12 @@ package io.github.pshevche.spokk.plugin
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.ir.declarations.IrModuleFragment
-import org.jetbrains.kotlin.ir.util.dump
 
 internal class SpokkIrGenerationExtension : IrGenerationExtension {
     override fun generate(
         moduleFragment: IrModuleFragment,
         pluginContext: IrPluginContext
     ) {
-        println(moduleFragment.dump())
+        println("[PSHEVCHE] Applying SpokkIrGenerationExtension")
     }
 }
