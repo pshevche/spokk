45d20c938cf0810443283ad8e93559893e528842
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-05-31 21:43:04 +0200

    Set up dogfooding of the "spokk" test engine

--- spokk-specs/src/test/kotlin/io/github/pshevche/spokk/compilation/BaseGradlePluginTest.kt
+++ spokk-specs/src/test/kotlin/io/github/pshevche/spokk/compilation/BaseGradlePluginTest.kt
@@ -3,17 +3,10 @@ package io.github.pshevche.spokk.compilation
 import io.github.pshevche.spokk.fixtures.Workspace
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
-import kotlin.test.BeforeTest
 
 open class BaseGradlePluginTest {
 
     val workspace = Workspace()
-
-    @BeforeTest
-    fun setup() {
-        workspace.configurePluginRepositories()
-        workspace.applySpokkPlugin()
-    }
 
     fun run(vararg args: String): BuildResult {
         return GradleRunner.create()
