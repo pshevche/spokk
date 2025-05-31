c390231f8715c925fb66a503b949e9c46a9af7f3
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-05-31 21:43:04 +0200

    Set up dogfooding of the "spokk" test engine

--- spokk-specs/build.gradle.kts
+++ spokk-specs/build.gradle.kts
@@ -1,15 +1,26 @@
+@file:OptIn(ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+
 plugins {
     `java-test-fixtures`
+    alias(libs.plugins.kotlin.power.assert)
     id("spokk.compiler-plugin-consumer")
     id("spokk.plugin-under-test-consumer")
 }
 
 dependencies {
     testImplementation(gradleTestKit())
-    // TODO: dogfood spokk instead
-    testImplementation(kotlin("test"))
+    testImplementation(projects.spokkCore)
 }
 
 tasks.test {
-    useJUnitPlatform()
+    useJUnitPlatform {
+        includeEngines.add("spokk")
+    }
+}
+
+powerAssert {
+    functions = setOf("kotlin.assert")
+    includedSourceSets = setOf("test")
 }
--- spokk-specs/src/test/kotlin/io/github/pshevche/spokk/compilation/SpokkGradlePluginSmokeTest.kt
+++ spokk-specs/src/test/kotlin/io/github/pshevche/spokk/compilation/SpokkGradlePluginSmokeTest.kt
@@ -1,11 +1,24 @@
 package io.github.pshevche.spokk.compilation
 
-import kotlin.test.Test
+import io.github.pshevche.spokk.lang.given
+import io.github.pshevche.spokk.lang.internal.FeatureMetadata
+import io.github.pshevche.spokk.lang.internal.SpecMetadata
+import io.github.pshevche.spokk.lang.then
+import io.github.pshevche.spokk.lang.`when`
 
+@SpecMetadata
 class SpokkGradlePluginSmokeTest : BaseGradlePluginTest() {
 
-    @Test
+    @FeatureMetadata
     fun `smoke test`() {
+        given
+        workspace.configurePluginRepositories()
+        workspace.applySpokkPlugin()
+
+        `when`
         run("tasks")
+
+        then
+        assert(true)
     }
 }
