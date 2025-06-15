26433bbbb6cc26878ce34a4c83f883afae96a6b4
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-06-15 11:43:20 +0200

    Add smoke tests for the test engine

---
+++ spokk-specs/src/test/kotlin/io/github/pshevche/spokk/runtime/engine/EngineTestKitUtils.kt
@@ -0,0 +1,15 @@
+package io.github.pshevche.spokk.runtime.engine
+
+import org.junit.platform.engine.DiscoverySelector
+import org.junit.platform.testkit.engine.EngineTestKit
+import org.junit.platform.testkit.engine.Events
+
+object EngineTestKitUtils {
+    fun execute(selector: DiscoverySelector): Events {
+        return EngineTestKit.engine("spokk")
+            .selectors(selector)
+            .execute()
+            .testEvents()
+            .debug()
+    }
+}
---
+++ spokk-specs/src/test/kotlin/io/github/pshevche/spokk/runtime/engine/SpokkTestEngineTest.kt
@@ -0,0 +1,79 @@
+package io.github.pshevche.spokk.runtime.engine
+
+import io.github.pshevche.spokk.fixtures.runtime.samples.SimpleSpec
+import io.github.pshevche.spokk.lang.internal.FeatureMetadata
+import io.github.pshevche.spokk.lang.internal.SpecMetadata
+import io.github.pshevche.spokk.runtime.engine.EngineTestKitUtils.execute
+import org.junit.platform.engine.UniqueId
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectClass
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectMethod
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage
+import org.junit.platform.engine.discovery.DiscoverySelectors.selectUniqueId
+
+@SpecMetadata
+class SpokkTestEngineTest {
+
+    @FeatureMetadata
+    fun `discovers test class by class name`() {
+        val events = execute(selectClass(SimpleSpec::class.java))
+        events.assertStatistics {
+            it.started(2).succeeded(1).failed(1)
+        }
+    }
+
+    @FeatureMetadata
+    fun `discovers test class by unique id`() {
+        val events =
+            execute(selectUniqueId(UniqueId.forEngine("spokk").append("spec", SimpleSpec::class.qualifiedName)))
+        events.assertStatistics {
+            it.started(2).succeeded(1).failed(1)
+        }
+    }
+
+    @FeatureMetadata
+    fun `discovers feature method by method name`() {
+        var events = execute(selectMethod(SimpleSpec::class.java, "successful feature"))
+        events.assertStatistics {
+            it.started(1).succeeded(1)
+        }
+
+        events = execute(selectMethod(SimpleSpec::class.java, "failing feature"))
+        events.assertStatistics {
+            it.started(1).failed(1)
+        }
+    }
+
+    @FeatureMetadata
+    fun `discovers feature method by unique id`() {
+        var events = execute(
+            selectUniqueId(
+                UniqueId.forEngine("spokk")
+                    .append("spec", SimpleSpec::class.qualifiedName)
+                    .append("feature", "successful feature")
+            )
+        )
+        events.assertStatistics {
+            it.started(1).succeeded(1)
+        }
+
+        events = execute(
+            selectUniqueId(
+                UniqueId.forEngine("spokk")
+                    .append("spec", SimpleSpec::class.qualifiedName)
+                    .append("feature", "failing feature")
+            )
+        )
+        events.assertStatistics {
+            it.started(1).failed(1)
+        }
+    }
+
+    @FeatureMetadata
+    fun `supports package selectors`() {
+        val events = execute(selectPackage(SimpleSpec::class.java.packageName))
+        events.assertStatistics {
+            it.started(4).succeeded(2).failed(2)
+        }
+    }
+
+}
