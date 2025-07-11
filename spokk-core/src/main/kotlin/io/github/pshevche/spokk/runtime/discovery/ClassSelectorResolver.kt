a6375fc473da53d79d607156337735a5a63be6ad
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-07-11 14:40:15 +0200

    Add license header to all production sources

--- spokk-core/src/main/kotlin/io/github/pshevche/spokk/runtime/discovery/ClassSelectorResolver.kt
+++ spokk-core/src/main/kotlin/io/github/pshevche/spokk/runtime/discovery/ClassSelectorResolver.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.pshevche.spokk.runtime.discovery
 
 import io.github.pshevche.spokk.runtime.SpokkException
@@ -6,6 +20,7 @@ import io.github.pshevche.spokk.runtime.node.FeatureNode
 import io.github.pshevche.spokk.runtime.node.SpecInfo
 import io.github.pshevche.spokk.runtime.node.SpecNode
 import io.github.pshevche.spokk.runtime.util.SpecUtil.isRunnableSpec
+import java.util.*
 import org.junit.platform.commons.support.ReflectionSupport
 import org.junit.platform.engine.TestDescriptor
 import org.junit.platform.engine.UniqueId
@@ -14,7 +29,6 @@ import org.junit.platform.engine.discovery.UniqueIdSelector
 import org.junit.platform.engine.support.discovery.SelectorResolver
 import org.junit.platform.engine.support.discovery.SelectorResolver.Match
 import org.junit.platform.engine.support.discovery.SelectorResolver.Resolution
-import java.util.Optional
 
 /**
  * Resolve class- and uniqueId-based selectors.
