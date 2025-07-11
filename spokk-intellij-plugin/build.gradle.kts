cedb0e6d47d76192387ddd84fccd8f94102df2a5
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-07-11 15:25:17 +0200

    Add IntelliJ plugin scaffolding

---
+++ spokk-intellij-plugin/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.intellij.platform)
+    id("spokk.kotlin-library")
+}
+
+group = "io.github.pshevche.spokk"
+version = "0.1"
+
+repositories {
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        create("IC", "2025.1")
+        testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
+    }
+}
+
+val releaseNotesFile = layout.projectDirectory.dir("docs").file("release-notes.txt")
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "251"
+        }
+
+        changeNotes = releaseNotesFile.asFile.readText()
+    }
+}
